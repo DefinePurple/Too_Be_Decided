45bd42089bcb81f0bdb5affbfb318949cbab5d4a
Daniel Fitzpatrick <danieljfitzpatrick3696@gmail.com>
2017-03-15 21:56:13 +0000

    Tutorial - Moving over to project

---
+++ LearningUnityFPSTutorial/Assets/Scripts/Gun/Gun_ApplyDamage.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TBD{
+	public class Gun_ApplyDamage : MonoBehaviour {
+
+        private Gun_Master GunMaster;
+        public int damage = 10;
+	}
+}
+
---
+++ LearningUnityFPSTutorial/Assets/Scripts/Gun/Gun_MuzzleFlash.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TBD{
+	public class Gun_MuzzleFlash : MonoBehaviour {
+
+        public ParticleSystem muzzleFlash;
+        private Gun_Master gunMaster;
+
+        void OnEnable() {
+            SetInitial();
+            gunMaster.EventPlayerInput += PlayMuzzleFlash; 
+        }
+
+        void OnDisable() {
+            gunMaster.EventPlayerInput -= PlayMuzzleFlash;
+        }
+
+        void SetInitial() {
+            gunMaster = GetComponent<Gun_Master>();
+        }
+
+        void PlayMuzzleFlash() {
+            if(muzzleFlash != null) {
+                muzzleFlash.Play();
+            }
+        }
+	}
+}
+
