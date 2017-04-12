e6a396b20d7edbea223d396a0c92915756a8173a
DefinePurple <danieljfitzpatrick3696@gmail.com>
2017-04-12 19:52:30 +0100

    GameManager_Master script created - handles player going to menu

---
+++ Too_Be_Decided/Assets/Scripts/GameManager_Master.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TBD {
+    public class GameManager_Master : MonoBehaviour {
+
+        public delegate void GameManagerEventHandler();
+        public event GameManagerEventHandler MenuToggleEvent;
+
+        public bool isMenuOn;
+
+        public void CallEventMenuToggle() {
+            if (MenuToggleEvent != null) {
+                MenuToggleEvent();
+            }
+        }
+    }
+}
+
