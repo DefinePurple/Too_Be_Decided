a75e27bd286844b9baa32a9f84564413a561750d
DefinePurple <danieljfitzpatrick3696@gmail.com>
2017-04-12 20:59:57 +0100

    Player movement turned off when menu is toggled

---
+++ Too_Be_Decided/Assets/Scripts/GameManager/GameManager_References.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TBD {
+    public class GameManager_References : MonoBehaviour {
+
+        public static string _playerTag;
+
+        public static string _enemyTag;
+
+        public static GameObject _player;
+
+        void OnEnable() {
+            _playerTag = "Player";
+            _enemyTag = "Enemy";
+
+            _player = GameObject.FindGameObjectWithTag(_playerTag);
+        }
+
+        void OnDisable() {
+
+        }
+    }
+}
+
--- Too_Be_Decided/Assets/Scripts/GameManager/GameManager_TogglePlayer.cs
+++ Too_Be_Decided/Assets/Scripts/GameManager/GameManager_TogglePlayer.cs
@@ -1,16 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
-public class GameManager_TogglePlayer : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+namespace TBD {
+    public class GameManager_TogglePlayer : MonoBehaviour {
+
+        private GameManager_Master gameManagerMaster;
+        private FirstPersonController fpsScript;
+
+        void OnEnable() {
+            SetInitial();
+
+            gameManagerMaster.MenuToggleEvent += TogglePlayer;
+        }
+
+        void OnDisable() {
+            gameManagerMaster.MenuToggleEvent -= TogglePlayer;
+        }
+
+        void SetInitial() {
+            gameManagerMaster = GetComponent<GameManager_Master>();
+            fpsScript = GameObject.FindGameObjectWithTag("Player").GetComponent<FirstPersonController>();
+        }
+
+        void TogglePlayer() {
+            if (fpsScript != null)
+                fpsScript.enabled = !fpsScript.enabled;
+        }
+    }
 }
+
