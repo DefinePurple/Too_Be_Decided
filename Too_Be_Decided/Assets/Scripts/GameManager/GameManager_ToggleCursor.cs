3048ca4883073ef9c30dc4db4def1df3a9477f7f
DefinePurple <danieljfitzpatrick3696@gmail.com>
2017-04-12 21:03:08 +0100

    Lets hope unitys buggy cursor handling is working

--- Too_Be_Decided/Assets/Scripts/GameManager/GameManager_ToggleCursor.cs
+++ Too_Be_Decided/Assets/Scripts/GameManager/GameManager_ToggleCursor.cs
@@ -2,15 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager_ToggleCursor : MonoBehaviour {
+namespace TBD {
+    public class GameManager_ToggleCursor : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        private GameManager_Master gameManagerMaster;
+        private bool isCursorLocked = true;
+
+        void OnEnable() {
+            SetInitial();
+            gameManagerMaster.MenuToggleEvent += ToggleCursorState;
+        }
+
+        void OnDisable() {
+            gameManagerMaster.MenuToggleEvent -= ToggleCursorState;
+        }
+
+        void Update() {
+            CheckIfCursorShouldBeLocked();
+        }
+
+        void SetInitial() {
+            gameManagerMaster = GetComponent<GameManager_Master>();
+        }
+
+        void ToggleCursorState() {
+            isCursorLocked = !isCursorLocked;
+        }
+
+        void CheckIfCursorShouldBeLocked() {
+            if (isCursorLocked) {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            } else {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+        }
+    }
 }
