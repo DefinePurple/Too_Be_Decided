1396ee97996fb0088af06e2b3c165cc18bd7439c
DefinePurple <danieljfitzpatrick3696@gmail.com>
2017-04-16 00:26:18 +0100

    Player Ammo Box Created

---
+++ Too_Be_Decided/Assets/Scripts/Player/Player_AmmoBox.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TBD{
+	public class Player_AmmoBox : MonoBehaviour {
+
+        private Player_Master playerMaster;
+        private int ammoCurrentCarried;
+        public int maxQuantity;
+
+        void OnEnable() {
+            SetInitial();
+            playerMaster.EventPickedUpAmmo += PickedUpAmmo;
+        }
+
+        void OnDisable() {
+            playerMaster.EventPickedUpAmmo -= PickedUpAmmo;
+        }
+
+        void SetInitial() {
+            playerMaster = GetComponent<Player_Master>();
+        }
+
+        void PickedUpAmmo(int quantity) {
+            ammoCurrentCarried += quantity;
+                    
+            if(ammoCurrentCarried > maxQuantity)
+                ammoCurrentCarried = maxQuantity;
+
+            playerMaster.CallEventAmmoChanged();
+        }
+	}
+}
+
